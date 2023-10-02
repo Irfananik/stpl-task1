ab6d6c1d629f46020bbb400a87c171238a7ce545
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2023-10-02 22:31:12 +0600

    Project setup completed

---
+++ src/assets/Searchbar/Searchbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Searchbar.css'
+
+const Searchbar = () => {
+    return (
+        <div>
+            <h1>anik</h1>
+        </div>
+    );
+};
+
+export default Searchbar;
